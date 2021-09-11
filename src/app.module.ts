9458d43e746590f0cf6c73b362e0ecb9a1eb9491
Franco Serio <francoagustinserio@gmail.com>
2021-09-10 22:32:25 -0300

    Add config to read .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TwitchModule } from './twitch/twitch.module';
+import { DiscordModule } from './discord/discord.module';
+import { TwitterModule } from './twitter/twitter.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [TwitchModule, DiscordModule, TwitterModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
