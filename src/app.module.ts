857a4e53a4c8ff2b697b49fe3c69eb0b4248f753
Franco Serio <francoagustinserio@gmail.com>
2021-12-08 19:20:41 -0300

    Delete unused app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TwitchModule } from './twitch/twitch.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [TwitchModule, ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
