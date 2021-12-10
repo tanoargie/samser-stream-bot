02c0c98105e9ee2a2a7545e3478426154c293442
Franco Serio <francoagustinserio@gmail.com>
2021-12-10 00:00:12 -0300

    Fix twitter factory

--- src/twitter/twitter.factory.ts
+++ src/twitter/twitter.factory.ts
@@ -2,12 +2,15 @@ import { TwitterClient } from 'twitter-api-client';
 
 const twitterFactory = {
   provide: 'TWITTER_CLIENT',
-  useValue: new TwitterClient({
-    apiKey: process.env.TWITTER_CLIENT_API_KEY,
-    apiSecret: process.env.TWITTER_CLIENT_API_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN,
-    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  }),
+  useFactory: () => {
+    const twitterClient = new TwitterClient({
+      apiKey: process.env.TWITTER_CLIENT_API_KEY,
+      apiSecret: process.env.TWITTER_CLIENT_API_SECRET,
+      accessToken: process.env.TWITTER_ACCESS_TOKEN,
+      accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+    return twitterClient;
+  },
 };
 
 export default twitterFactory;
