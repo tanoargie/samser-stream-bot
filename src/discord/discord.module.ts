236d6a11b5717be30e03d8259be50add66e12653
Franco Serio <francoagustinserio@gmail.com>
2021-12-10 00:01:10 -0300

    ConfigModule does not seem necessary

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DiscordService } from './discord.service';
-import { ConfigModule } from '@nestjs/config';
 import DiscordFactory from './discord.factory';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '../../.env',
-    }),
-  ],
   providers: [DiscordService, DiscordFactory],
   exports: [DiscordService],
 })
--- src/twitter/twitter.module.ts
+++ src/twitter/twitter.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TwitterService } from './twitter.service';
-import { ConfigModule } from '@nestjs/config';
 import TwitterFactory from './twitter.factory';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '../../.env',
-    }),
-  ],
   providers: [TwitterService, TwitterFactory],
   exports: [TwitterService],
 })
